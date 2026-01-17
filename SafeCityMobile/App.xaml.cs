aca8c2395b8eec9c8cdb726c7e3677b989c0badc
Jakub Jachowicz <KubaJachowicz39@gmail.com>
2026-01-17 13:32:08 +0100

    init commit

---
+++ SafeCityMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SafeCityMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SafeCityMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SafeCityMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
